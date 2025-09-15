798941a4fc72b27a5defc16ff54c161c78f56bee
Sathwik M S <sathvikms344@gmail.com>
2025-09-15 22:34:08 +0530

    add news web articles

--- app.js
+++ app.js
@@ -10,3 +10,20 @@ menuBtn.addEventListener("click", () => {
         menuCtrl = true;
     }
 })
+
+
+
+
+// image clicking
+document.querySelectorAll(".main-art img").forEach(image => {
+  image.onclick = () => {
+    document.querySelector(".popup-image").style.display = "block";
+    document.querySelector(".popup-image img").src = image.getAttribute("src");
+  };
+});
+
+
+// x button
+document.querySelector(".popup-image span").onclick=() =>{
+    document.querySelector(".popup-image").style.display = "none";
+}
