f4210d6b31bd1d9aaabefd3b88970bd8bcaf4f65
Sathwik M S <sathvikms344@gmail.com>
2025-09-15 23:43:21 +0530

    more changes made

--- app.js
+++ app.js
@@ -10,20 +10,3 @@ menuBtn.addEventListener("click", () => {
         menuCtrl = true;
     }
 })
-
-
-
-
-// image clicking
-document.querySelectorAll(".main-art img").forEach(image => {
-  image.onclick = () => {
-    document.querySelector(".popup-image").style.display = "block";
-    document.querySelector(".popup-image img").src = image.getAttribute("src");
-  };
-});
-
-
-// x button
-document.querySelector(".popup-image span").onclick=() =>{
-    document.querySelector(".popup-image").style.display = "none";
-}
