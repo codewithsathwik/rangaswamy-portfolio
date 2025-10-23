5b4e1547135b6f011b8f21c0a07c4c7e21ec6c07
codewithsathwik <sathvikms344@gmail.com>
2025-10-23 20:26:32 +0530

    css changes

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 let navMb = document.querySelector("#nav-mb");
 let menuBtn = document.querySelector("#menu-btn");
 let menuCtrl = false;
+
 menuBtn.addEventListener("click", () => {
-    if (menuCtrl) {
-        navMb.style.display = "none";
-        menuCtrl = false;
-    } else {
-        navMb.style.display = "block";
-        menuCtrl = true;
-    }
+    if (!menuCtrl) {
+    navMb.classList.remove("ani-up");
+    navMb.classList.add("ani-dwn");
+    menuCtrl = true; // menu is now open
+  } else {
+    navMb.classList.add("ani-up");
+    navMb.classList.remove("ani-dwn");
+    menuCtrl = false; // menu is now closed
+  }
 })
+
+
+
+
+
 
 let loadpg = document.querySelector("#load-page");
 
-window.addEventListener("load",()=>{
+window.addEventListener("load", () => {
     loadpg.style.display = "none"
 })
+
+
+
